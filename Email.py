8dc01904b18855fd6f455e050288e413c18f4758
Anna <salatik339@mail.ru>
2020-04-22 04:14:54 +0300

    as a method

--- Email.py
+++ Email.py
@@ -1,46 +1,48 @@
 #! /usr/bin/env python3
-# def send_mail(listOfRecipients, listOfName):
-
-
-
-import smtplib
-
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-me = "salatik3392@yandex.ru"
 listOfRecipients = ['anna.karakhanova@yandex.ru', "salatik339@mail.ru"]
 listOfName = ['anna', 'salatik']
+def send_mail(listOfRecipients, listOfName):
 
-for listOfRecipients, listOfName in zip(listOfRecipients, listOfName):
-    msg = MIMEMultipart('multipart')
-    msg['Subject'] = "Hi, " + listOfName
-    msg['From'] = me
-    msg['To'] = listOfRecipients
+    import smtplib
 
-    #text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-    html = """\
-            <html>
-              <head></head>
-              <body>
-                <p>Hi, %s!<br>
-                </p>
-              </body>
-            </html>
-            """ %(listOfName)
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
 
-    #part1 = MIMEText(text, 'plain', 'utf-8')
-    part2 = MIMEText(html, 'html', 'utf-8')
+    me = "salatik3392@yandex.ru"
+    listOfRecipients = ['anna.karakhanova@yandex.ru', "salatik339@mail.ru"]
+    listOfName = ['anna', 'salatik']
 
-    #msg.attach(part1)
-    msg.attach(part2)
+    for listOfRecipients, listOfName in zip(listOfRecipients, listOfName):
+        msg = MIMEMultipart('multipart')
+        msg['Subject'] = "Hi, " + listOfName
+        msg['From'] = me
+        msg['To'] = listOfRecipients
 
-    try:
-        s = smtplib.SMTP('smtp.yandex.ru')
-        s.ehlo()
-        s.starttls()
-        s.login('salatik3392@yandex.ru', 'Atos2018*')
-        s.sendmail(me, listOfRecipients, msg.as_string())
-        s.quit()
-    except Exception as e:
-        print(f'Oh no! Something bad happened!\n {e}')
+        #text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+        html = """\
+                <html>
+                  <head></head>
+                  <body>
+                    <p>Hi, %s!<br>
+                    </p>
+                  </body>
+                </html>
+                """ %(listOfName)
+
+        #part1 = MIMEText(text, 'plain', 'utf-8')
+        part2 = MIMEText(html, 'html', 'utf-8')
+
+        #msg.attach(part1)
+        msg.attach(part2)
+
+        try:
+            s = smtplib.SMTP('smtp.yandex.ru')
+            s.ehlo()
+            s.starttls()
+            s.login('salatik3392@yandex.ru', 'Atos2018*')
+            s.sendmail(me, listOfRecipients, msg.as_string())
+            s.quit()
+        except Exception as e:
+            print(f'Oh no! Something bad happened!\n {e}')
+
+send_mail(listOfRecipients, listOfName)
